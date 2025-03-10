c84d0a43d882a559bd998c8e968a354188c8e597
Reichenbach, Michael <755327+Silthus@users.noreply.github.com>
2025-03-10 13:37:43 +0100

    fix: run depency build before pre-commit

--- bin/pre-commit-hooks/helm-lint.sh
+++ bin/pre-commit-hooks/helm-lint.sh
@@ -11,5 +11,5 @@ fi
 for chart in $(find . -name 'Chart.yaml'); do
     chart_dir=$(dirname "$chart")
     echo "Running linting in $chart_dir"
-    (cd "$chart_dir" && helm lint . --set-string project=my-project --set-string name=my-service)
+    (cd "$chart_dir" && helm dependency build && helm lint . --set-string project=my-project --set-string name=my-service)
 done
